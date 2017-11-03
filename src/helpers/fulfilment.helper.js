53c998fcbf29ef45117db794a674c3bef9947878
mewild <michael@wildapplications.com>
2017-11-03 13:33:06 +0000

    new route

--- src/helpers/fulfilment.helper.js
+++ src/helpers/fulfilment.helper.js
@@ -179,7 +179,7 @@ helper.getOrderBreakdown = function(call, callback){
         Order.aggregate([
           { $match: { $and: [
             {status: {$in: ['COMPLETE', 'CANCELLED']}},
-            {premises: {$in: [result._id]}}
+            {premises: result._id.toString()}
           ]}}
         ]).exec(function(err, orders){
           if(err){
