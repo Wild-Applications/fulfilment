21a9784db06832aabeb217fd3ce5805ed9ba57ba
mewild <michael@wildapplications.com>
2017-12-08 14:59:49 +0000

    Statistics

--- src/models/order.schema.js
+++ src/models/order.schema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 
 var schema = new mongoose.Schema({
-  subtotal: {type: Number, required: true, default: 0, get: getPrice, set: setPrice},
+  subtotal: {type: Number, required: true, default: 0},
   owner: { type : Number, required : true, index: true},
   products: [{type: mongoose.Schema.ObjectId, ref: 'Product'}],
   table: {type: mongoose.Schema.ObjectId, required: true, ref:'Table'},
